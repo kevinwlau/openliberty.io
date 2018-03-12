28c0963ab5f391842045f5523d27f98f47f0aeef
Kevin Lau <laukevin@us.ibm.com>
2018-03-12 11:58:54 -0500

    Use latest version of CF CLI

--- .travis/cf-blue-green.sh
+++ .travis/cf-blue-green.sh
@@ -3,9 +3,9 @@ set -e
 
 echo "============== INSTALLING CLOUD FOUNDRY CLI CLIENT =============="
 # https://github.com/cloudfoundry/cli/releases
-wget --max-redirect=1 --output-document=cf_cli_6.26.0.tgz "https://cli.run.pivotal.io/stable?release=linux64-binary&version=6.26.0&source=github-rel"
-gunzip cf_cli_6.26.0.tgz
-tar -xvf cf_cli_6.26.0.tar
+wget --max-redirect=1 --output-document=cf_cli_6.35.0.tgz "https://cli.run.pivotal.io/stable?release=linux64-binary&version=6.35.0&source=github-rel"
+gunzip cf_cli_6.35.0.tgz
+tar -xvf cf_cli_6.35.0.tar
 
 echo "============== LOGGING INTO CLOUD FOUNDRY =============="
 ./cf login -a=$BLUEMIX_API -s=$BLUEMIX_SPACE -o=$BLUEMIX_ORGANIZATION -u=$BLUEMIX_USER -p=$BLUEMIX_PASSWORD
