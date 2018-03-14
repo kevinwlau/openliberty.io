d7383a3b48740370e59d6543ca7f21ac265bc6e0
Kevin Lau <laukevin@us.ibm.com>
2018-03-14 10:23:02 -0500

    Remove site-check and re-routing cuz using same domain.

--- .travis/cf-blue-green.sh
+++ .travis/cf-blue-green.sh
@@ -31,13 +31,13 @@ DOMAIN=$(cat $MANIFEST | grep domain: | awk '{print $2}')}
 ./cf push $GREEN -p ./target/openliberty.war -f $MANIFEST -b liberty-for-java
 
 # ensure it starts
-echo "Checking status of new instance..."
-curl --fail -s -I "https://${GREEN}.${DOMAIN}"
+# echo "Checking status of new instance..."
+# curl --fail -s -I "https://${GREEN}.${DOMAIN}"
 
 # add the GREEN application to each BLUE route to be load-balanced
 # TODO this output parsing seems a bit fragile...find a way to use more structured output
-echo "Rerouting main site..."
-./cf routes | tail -n +4 | grep $BLUE | awk '{print $3" -n "$2}' | xargs -n 3 ./cf map-route $GREEN
+# echo "Rerouting main site..."
+# ./cf routes | tail -n +4 | grep $BLUE | awk '{print $3" -n "$2}' | xargs -n 3 ./cf map-route $GREEN
 
 # cleanup
 # TODO consider 'stop'-ing the BLUE instead of deleting it, so that depedencies are cached for next time
